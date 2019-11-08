40c4c18e8997699b3af72278e56afcfb8a5e57f2
Walter Prorok <waltprorok@gmail.com>
2019-11-07 23:32:00 -0500

    formated html

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -17,7 +17,6 @@
 
 @yield('content')
 
-
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
